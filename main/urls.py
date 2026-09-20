a6d8217237b1c5e82452281ab229c5b6e98c9464
xhenst <kaylalifahk@gmail.com>
2026-09-20 23:23:56 +0700

    implement JSON data delivery, delete project feature, and modal UI

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education,  create_education, get_education_json, delete_education, create_project,show_projects
+from main.views import *
 app_name = "main"
 
 urlpatterns = [
@@ -12,6 +12,8 @@ urlpatterns = [
     path("education/<uuid:education_id>/delete/", delete_education, name="delete_education"),
     path("projects/add/", create_project, name="create_project"),
     path("projects/", show_projects, name="show_projects"),
+    path("api/projects/", get_projects_json, name="get_projects_json"),
+    path("projects/<uuid:project_id>/delete/",delete_project,name="delete_project"),
 ]
 
 
--- main/views.py
+++ main/views.py
@@ -90,8 +90,36 @@ def create_project(request):
     }
     return render(request, "projects_form.html", context)
 def show_projects(request):
+    json_response = get_projects_json(request)
+
+    projects = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    projects = [project.object for project in projects]
+    title_query = request.GET.get("title", "").strip()
+
     context = {
         "name": "Kayla Alifah Khairunisa",
-        "project_list": Project.objects.all(),
+        "project_list": projects,
+        "title_query": title_query,
     }
     return render(request, "project.html", context)
+def get_projects_json(request):
+    title_query = request.GET.get("title", "").strip()
+    projects = Project.objects.all()
+
+    if title_query:
+        projects = projects.filter(title__icontains=title_query)
+
+    projects_json = serializers.serialize("json", projects)
+    return HttpResponse(projects_json, content_type="application/json")
+def delete_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+
+    if request.method == "POST":
+        project.delete()
+        messages.success(request, "Project berhasil dihapus!")
+        return redirect("main:show_projects")
+
+    return redirect("main:show_projects")
